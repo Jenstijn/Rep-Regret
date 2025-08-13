d41152de75959bb8a78565e5b17b22dab6859e6d
Jenstijn <tom@dohle.nl>
2025-08-13 21:07:32 +0000

    update index weghalen linksbovenin

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //service-worker.js
-const CACHE = 'repregret-v1'
+const CACHE = 'repregret-v2'
 const BASE = new URL('.', self.location).href  // eindigt op .../Rep-Regret/
 
 function abs(path) { return new URL(path, BASE).toString() }
